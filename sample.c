c9657bdd5f2218e9fa02f7b2cdd3e50138906ce6
Symisc Systems <chm@symisc.net>
2017-11-07 04:16:05 +0100

    Intro to the ASCII Art C/C++ interfaces

--- sample.c
+++ sample.c
@@ -1,34 +1,35 @@
 /*
- * Compile this file together with the ASCII Art source code
- * to generate the executable. For example: 
- *
- *  gcc -W -Wall -O6 ascii_art.c sample.c -o ascii
- * ./ascii test.png
- *
- * If you get a compile-time error, that mean that the `ascii_art.hex` model is missing.
- * Download it from: https://pixlab.io/art
- */
-
+* Compile this file together with the ASCII Art source code
+* to generate the executable. For example:
+*
+*  gcc -W -Wall -O6 ascii_art.c sample.c -o ascii -D ART_ENABLE_STB_IMAGE
+* ./ascii test.png
+*
+* ART_ENABLE_STB_IMAGE directive must be defined in order to load images from disk.
+*
+* If you get a compile-time error, it means that the `ascii_art.hex` model is missing.
+* Download it from: https://pixlab.io/art
+*/
 #include <stdio.h>
 #include <stdlib.h>
-#include "ascii_art.h"
+#include "ascii_art/ascii_art.h"
 
 int main(int argc, char **argv) {
 	ascii_render sRender; /* Stack allocated */
-	
+
 	unsigned char *zText, *zBlob;
 	int width, height;
 	unsigned int nBytes;
-	
-	if( argc < 2 ){
+
+	if (argc < 2) {
 		puts("Missing input image");
 		return -1;
-	} 
-	
+	}
+
 	/* Initialize the render structure */
 	AsciiArtInit(&sRender);
 
-	/* Load an image from disk */
+	/* Load the target image */
 	zBlob = AsciiArtLoadImage(argv[1], &width, &height);
 	if (zBlob == 0) {
 		puts("Cannot load image");
@@ -49,6 +50,6 @@ int main(int argc, char **argv) {
 	/* Release memory */
 	free(zText);
 	free(zBlob);
-	
+
 	return 0;
 }
